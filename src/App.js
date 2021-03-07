47c92cc7d607d65064dfcae022c800376306014a
yuezhou777 <zhou.yue2@husky.neu.edu>
2021-03-08 01:55:36 +0800

    react assignment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link } from "react-router-dom";
 import DynamicArticle from "./DynamicArticle/DynamicArticle.jsx";
+import ArticleList from "./ArticleList/ArticleList";
 import { isEmpty } from "lodash";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       // performs a GET request
       const response = await fetch("http://demo1390455.mockable.io/articles");
       const responseJson = await response.json();
+      console.log("Response Json: ", responseJson);
       setFetchedData(responseJson);
     };
 
@@ -19,21 +21,36 @@ function App() {
     }
   }, [fetchedData]);
 
-  return isEmpty(fetchedData) ? null : (
+  return isEmpty(fetchedData) ? <div>You have no data!</div> : (
     <div className="App">
       <Switch>
-        <Route exact path={`/articlelist`}></Route>
+        <Route exact path={`/articlelist`}>
+          <ArticleList articles={Object.values(fetchedData)}></ArticleList>
+        </Route>
+
         <Route
           path={`/articlelist/:slug`}
           render={({ match }) => {
             // getting the parameters from the url and passing
             // down to the component as props
-            console.log("this slug", match.params.slug);
-            return <div>Component</div>;
+            const thisSlug = match.params.slug;
+            console.log("this slug", thisSlug);
+            let index = 0;
+            Object.values(fetchedData).map((cont, idx) => {
+              if(cont.slug === thisSlug) {
+                index = idx;
+              }
+            })
+            return <DynamicArticle article={Object.values(fetchedData)[index]} />;
           }}
         />
+
         <Route>
           <DynamicArticle article={Object.values(fetchedData)[1]} />
+        </Route>
+
+        <Route>
+          <Link to="/articlelist">Go to article list</Link>
         </Route>
       </Switch>
     </div>
---
+++ src/ArticleList/ArticleList.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ArticleListItem from "../ArticleListItem/ArticleListItem.jsx";
+
+const ArticleList = (props) => {
+
+    const articleList = props.articles.map((article) => (
+        <ArticleListItem key={article.slug} article={article} />
+    ))
+
+    return (
+        <div>
+            {articleList}
+        </div>
+    )
+
+}
+
+export default ArticleList;
